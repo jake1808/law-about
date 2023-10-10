ea3b38405d5554bf5f3fbfeee2235e1d1ce5170a
Jacob Mutale <51802968+jake1808@users.noreply.github.com>
2023-10-10 09:58:46 +0000

    overflow-x hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className='scroll-smooth overflow-x-hidden'>
 
       <body className={roboto.className}>
         <NavBar />
